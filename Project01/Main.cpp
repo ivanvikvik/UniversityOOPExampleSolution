5e3cb4734aa1f117569fb8929e369839bae595ad
ivanvikvik <ivanvikvik@gmail.com>
2024-04-01 19:19:17 +0300

    Add new way to create objects (применили ещё один способ создания объектов)

--- Project01/Main.cpp
+++ Project01/Main.cpp
@@ -2,23 +2,11 @@
 #include "Student.h"
 
 int main() {
-	Student st1("Alex");
-	Student st2("Potter");
-	Student st3("Alice");
-	Student st4;
+	Student st1 = Student("Alex");
+	Student st2 = Student("Potter", 14);
+	Student st3 = Student("Alice", 15, 10);
+	Student st4 = Student();
 		
-	/*st1.name = "Alex";
-	st1.age = 14;
-	st1.mark = 9;
-
-	st2.name = "Potter";
-	st2.age = 16;
-	st2.mark = 10;
-
-	st3.name = "Alice";
-	st3.age = 13;
-	st3.mark = 7;*/
-	
 	cout << st1.convert() << endl;
 	cout << st2.convert() << endl;
 	cout << st3.convert() << endl;
