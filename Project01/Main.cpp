89b6465d7651fdbd2a68c700088862d7a2203bc7
ivanvikvik <ivanvikvik@gmail.com>
2024-04-24 19:00:29 +0300

    Add new project for Abstract Data Structure (добавление нового проекта для реализации абстрактных структур данных)

--- Project01/Main.cpp
+++ Project01/Main.cpp
@@ -2,25 +2,7 @@
 #include "Manager.h"
 
 int main() {
-
-	Group groupA;
-
-	Student st1{ "Alex",14,10 };
-	Student st2{ "Peter",16,9 };
-	Student st3{ "Alice" };
-
-	groupA.add(st1);
-	groupA.add(st2);
-	groupA.add(st3);
-
-
-	cout << "Before remove:" << endl;
-	cout << groupA.convertToString() << endl;
-
-	groupA.remove(st3);
-
-	cout << "After remove:" << endl;
-	cout << groupA.convertToString() << endl;
+	
 
 	return 0;
 }
