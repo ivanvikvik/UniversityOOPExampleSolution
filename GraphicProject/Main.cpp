1f9afa83523ce9758ae1e103165fc13391ca59f7
ivanvikvik <ivanvikvik@gmail.com>
2024-05-29 18:59:05 +0300

    Use typeid() operator

--- GraphicProject/Main.cpp
+++ GraphicProject/Main.cpp
@@ -6,13 +6,20 @@
 using namespace std;
 
 int main() {
-	Rectangle* rect = new Rectangle(4, 5, 1, 1);
-	Circle* circle = new Circle(5, 3, 4);
+	Figure* rect = new Rectangle(4, 5, 1, 1);
+	Figure* circle = new Circle(5, 3, 4);
 
 	Editor editor;
 
-	cout << editor.justDoIt(rect) << endl;
-	cout << editor.justDoIt(circle) << endl;
+	cout << typeid(rect).name() << endl;
+	cout << typeid(circle).name() << endl;
+
+	cout << typeid(*rect).name() << endl;
+	cout << typeid(*circle).name() << endl;
+
+	cout << strcmp(typeid(*rect).name(), "class Rectangle") << endl;
+	cout << strcmp(typeid(*circle).name(), "class Circle") << endl;
 
 	return 0;
 }
+
